6f742746c9799cd8f1b901426e0277753aef8b65
lourvaldez <162723167+lourvaldez@users.noreply.github.com>
2024-05-24 01:06:05 -0300

    Update loops_and_print.py

--- loops_and_print.py
+++ loops_and_print.py
@@ -1,6 +1,26 @@
-def enumerate_list(list):
-    return list
+def enumerate_list(lista):
+    #nueva lista para no modificar la primera
+    nueva_lis=[]
+    indice = 0
+    for elemento in lista:
+        if elemento!= "":
+            nueva_lis.append(f"{indice}. {elemento}")
+            indice = indice + 1
+    return nueva_lis
+
+colors = ["Red", "Green", "", "White", "Black"]
+enumerate_list(colors)
+
+
+
 
 
 def enumerate_backwards(list):
-    return list
+    nueva_lis = []
+    indice = 0
+    for elemento in list:
+        elemento= elemento[::-1]
+        if elemento != "":
+            nueva_lis.append(f"{indice}. {elemento}")
+            indice = indice + 1
+    return nueva_lis
