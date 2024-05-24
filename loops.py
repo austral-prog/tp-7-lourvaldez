eaed8540c4fe58c08fca3adf1c94d66eaee61f65
lourvaldez <162723167+lourvaldez@users.noreply.github.com>
2024-05-24 09:14:33 -0300

    Update loops.py

--- loops.py
+++ loops.py
@@ -1,26 +1,52 @@
-def enumerate_list(lista):
-    #nueva lista para no modificar la primera
-    nueva_lis=[]
-    indice = 0
-    for elemento in lista:
-        if elemento!= "":
-            nueva_lis.append(f"{indice}. {elemento}")
-            indice = indice + 1
-    return nueva_lis
+def index_of_by_index(word, list, index):
+    for position, element in enumerate(list):
+        if element == word and index <= position :
+            return position
+    else:
+        return -1
+
 
 colors = ["Red", "Green", "", "White", "Black"]
-enumerate_list(colors)
+for position, element in enumerate(colors):
+    print(f"{position}: {element}")
 
 
 
 
 
-def enumerate_backwards(list):
-    nueva_lis = []
-    indice = 0
-    for elemento in list:
-        elemento= elemento[::-1]
-        if elemento != "":
-            nueva_lis.append(f"{indice}. {elemento}")
-            indice = indice + 1
-    return nueva_lis
+def index_of_empty(list):
+    for position, element in enumerate(list):
+        if element == "":
+            return position
+    else:
+        return -1
+
+
+def index_of(word, list):
+    index= 0
+    for element in list:
+        if element == word:
+            return index
+        index += 1
+    else:
+            return -1
+
+
+
+def put(word, list):
+    for position, element in enumerate(list):
+        if element == "":
+            list[position] = word
+            return position
+    else:
+        return -1
+
+
+
+def remove(word, list):
+    repetidos = 0
+    for position, element in enumerate(list):
+        if word==element:
+            list[position] = ""
+            repetidos += 1
+    return repetidos
